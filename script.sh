e27ac2fcc7f51c9244d1c53d1d5bb7e771ac3c44
stepney141 <stepney141andi.c.i@gmail.com>
2020-10-19 13:57:23 +0900

    feat: added ISO-8601 information into commit comments

--- script.sh
+++ script.sh
@@ -9,6 +9,7 @@ wget --no-check-certificate \
     | jq '.[] | [ .full_name, .html_url, .description, .stargazers_count ] | @csv' -r \
 >> starred_repos.csv
 
+date="`date --iso-8601=minutes`"
 git add .
-git commit -m "updated starred_repos.csv"
+git commit -m "updated starred_repos.csv: $date"
 git push origin master
