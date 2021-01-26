5f1432cbea7f5c2bbc59c3046aea1ac419d66318
stepney141 <stepney141andi.c.i@gmail.com>
2021-01-26 22:12:27 +0900

    feat: complete Qiita LGTM Extractor

--- qiita_lgtm/puppeteer_qiita.js
+++ qiita_lgtm/puppeteer_qiita.js
@@ -4,7 +4,9 @@ require("dotenv").config();
 
 // vars for qiita
 const userid = "stepney141";
+let page_max;
 let page_num = 1;
+let urldata = [];
 
 // vars for twitter
 const user_name = process.env.TWITTER_ACCOUNT;
@@ -13,26 +15,33 @@ const password = process.env.TWITTER_PASSWORD;
 async function getLgtm(browser) {
   let page = await browser.newPage();
 
-  await page.goto(`https://qiita.com/${userid}/lgtms?page=${page_num}`, {
-    waitUntil: "domcontentloaded",
-  });
-  await page.screenshot({ path: `scroll-${page_num}.png` });
-  const page_max = 22; // 仮設定、本来は要素を取得して数値を代入
-  page_num++;
-
-  while (page_max >= page_num) {
+  do {
     await page.goto(`https://qiita.com/${userid}/lgtms?page=${page_num}`, {
-      waitUntil: "domcontentloaded",
+      waitUntil: "networkidle2",
     });
-    // await page.screenshot({ path: `scroll-${page_num}.png` });
 
-    // const data = await page.$x(
-    //   "/html/body/div[1]/div[3]/div/div[2]/div/div/div[3]/div/div[2]/a[2]"
-    //   "/html/body/div[1]/div[3]/div/div[2]/div/div/div[3]/div[39]/div[2]/a[2]"
-    // );
+    if (page_num == 1) {
+      // get max cursor number
+      const elementHandles = await page.$x(
+        "/html/body/div[1]/div[3]/div/div[2]/div/div/div[3]/ul/li[2]/span"
+      );
+      page_max = Number(
+        (
+          await (await elementHandles[0].getProperty("innerHTML")).jsonValue()
+        ).substr(-2, 2)
+      );
+    }
+
+    // get article urls
+    const articleDataHandles = await page.$x(
+      '/html/body/div[1]/div[3]/div/div[2]/div/div/div[3]/div/div[2]/a[contains(@href, "qiita.com")]'
+    );
+    for (const data of articleDataHandles) {
+      urldata.push(await (await data.getProperty("href")).jsonValue());
+    }
 
     page_num++;
-  }
+  } while (page_max >= page_num);
 }
 
 // Log in to qiita before scraping in order to avoid annoying prompts that recommend creating a new account
@@ -43,10 +52,10 @@ async function qiitaLogin(browser) {
   await page.goto("https://qiita.com/login", {
     waitUntil: "networkidle2",
   });
-  const qiitaLoginButtonElement = await page.$x(
+  const qiitaLoginButtonHandle = await page.$x(
     "/html/body/div[1]/div/div[1]/div/div[2]/div[1]/form[2]/button"
   );
-  await qiitaLoginButtonElement[0].click();
+  await qiitaLoginButtonHandle[0].click();
   await page.waitForNavigation({
     timeout: 60000,
     waitUntil: "networkidle2",
@@ -62,18 +71,42 @@ async function qiitaLogin(browser) {
   });
 }
 
+async function output(data) {
+  try {
+    await fs.writeFile(
+      "./lgtm_article_url.json",
+      JSON.stringify(data, null, "\t"),
+      (e) => {
+        if (e) console.log("error: ", e);
+      }
+    );
+    console.log("output completed");
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
 (async () => {
+  const startTime = Date.now();
+
   const browser = await puppeteer.launch({
     defaultViewport: {
       width: 600,
       height: 700,
     },
-    headless: false,
+    headless: true,
     slowMo: 50,
   });
 
   await qiitaLogin(browser);
   await getLgtm(browser);
+  await output(urldata);
+
+  console.log(
+    "The processsing took " +
+      Math.round((Date.now() - startTime) / 1000) +
+      " seconds"
+  );
 
   await browser.close();
 })();
