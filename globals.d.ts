8c11b74fc9c87fa050554b2a5a9e764cad1951a1
stepney141 <stepney141andi.c.i@gmail.com>
2023-10-01 02:40:12 +0900

    fix: 環境変数の型を整備

---
+++ globals.d.ts
@@ -0,0 +1,23 @@
+// environment variables
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV?: string;
+    readonly BOOKMETER_ACCOUNT?: string;
+    readonly BOOKMETER_PASSWORD?: string;
+    readonly BOUNDHUB_ACCOUNT?: string;
+    readonly BOUNDHUB_PASSWORD?: string;
+    readonly CINII_API_APPID?: string;
+    readonly NOTE_ACCOUNT?: string;
+    readonly NOTE_PASSWORD?: string;
+    readonly TERATAIL_API_TOKEN?: string;
+    readonly TWITTER_ACCOUNT?: string;
+    readonly TWITTER_PASSWORD?: string;
+    readonly ZENN_GOOGLE_ACCOUNT?: string;
+    readonly ZENN_GOOGLE_PASSWORD?: string;
+    readonly WIKIPEDIA_USERNAME?: string;
+    readonly WIKIPEDIA_PASSWORD?: string;
+    readonly OAUTH_TOKEN_OF_GITHUB?: string;
+    readonly NICONICO_ACCOUNT?: string;
+    readonly NICONICO_PASSWORD?: string;
+  }
+}
