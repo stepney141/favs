40e6d2fb07a9c4b6a2b90a0dee6def38abe4e891
stepney141 <stepney141andi.c.i@gmail.com>
2023-12-08 20:26:19 +0900

    fix: [bookmeter_wish] シェルスクリプトのバグ取り

--- bookmeter_wish/is_in_my_lib.sh
+++ bookmeter_wish/is_in_my_lib.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
 
 base_columns="isbn_or_asin, book_title, author, publisher, published_date"
+filter_book_title_error="(book_title NOT LIKE 'Not_found_in%' AND book_title NOT LIKE '%INVALID_ISBN%')"
 
 # 上智大
-q -d, -O -H "SELECT $base_columns FROM ./bookmeter_wish_books.csv \
-WHERE exist_in_Sophia='No' AND (book_title NOT LIKE 'Not_found_in%' OR book_title NOT LIKE 'INVALID_ISBN')"\
+q -d, -O -H "SELECT $base_columns \
+FROM ./bookmeter_wish_books.csv \
+WHERE exist_in_Sophia='No' AND $filter_book_title_error"\
 > not_in_Sophia.csv
 
 q -d, -O -H "SELECT $base_columns, central_opac_link, mathlib_opac_link \
-FROM ./bookmeter_wish_books.csv WHERE exist_in_Sophia='Yes'"\
+FROM ./bookmeter_wish_books.csv \
+WHERE exist_in_Sophia='Yes'"\
 > in_Sophia.csv
 
 # 東大
-q -d, -O -H "SELECT $base_columns FROM ./bookmeter_wish_books.csv \
-WHERE exist_in_UTokyo='No' AND (book_title NOT LIKE 'Not_found_in%' OR book_title NOT LIKE 'INVALID_ISBN')"\
+q -d, -O -H "SELECT $base_columns \
+FROM ./bookmeter_wish_books.csv \
+WHERE exist_in_UTokyo='No' AND $filter_book_title_error"\
 > not_in_UTokyo.csv
 
-q -d, -O -H "SELECT $base_columns, central_opac_link FROM ./bookmeter_wish_books.csv \
-WHERE exist_in_UTokyo='Yes' AND (book_title NOT LIKE 'Not_found_in%' OR book_title NOT LIKE 'INVALID_ISBN')"\
+q -d, -O -H "SELECT $base_columns, central_opac_link \
+FROM ./bookmeter_wish_books.csv \
+WHERE exist_in_UTokyo='Yes' AND $filter_book_title_error"\
 > in_UTokyo.csv
 
 # 上智にあって東大にない
-q -d, -O -H "SELECT $base_columns FROM ./bookmeter_wish_books.csv \
-WHERE exist_in_Sophia='Yes' AND exist_in_UTokyo='No' AND (book_title NOT LIKE 'Not_found_in%' OR book_title NOT LIKE 'INVALID_ISBN')"\
+q -d, -O -H "SELECT $base_columns \
+FROM ./bookmeter_wish_books.csv \
+WHERE exist_in_Sophia='Yes' AND exist_in_UTokyo='No' AND $filter_book_title_error"\
 > in_Sophia_but_not_in_UTokyo.csv
 
 # 東大にあって上智にない
-q -d, -O -H "SELECT $base_columns FROM ./bookmeter_wish_books.csv \
-WHERE exist_in_Sophia='No' AND exist_in_UTokyo='Yes' AND (book_title NOT LIKE 'Not_found_in%' OR book_title NOT LIKE 'INVALID_ISBN')"\
+q -d, -O -H "SELECT $base_columns \
+FROM ./bookmeter_wish_books.csv \
+WHERE exist_in_Sophia='No' AND exist_in_UTokyo='Yes' AND $filter_book_title_error"\
 > in_UTokyo_but_not_in_Sophia.csv
 
 # 上智にも東大にもない
-q -d, -O -H "SELECT $base_columns, exist_in_Sophia, exist_in_UTokyo FROM ./bookmeter_wish_books.csv \
-WHERE exist_in_Sophia='No' AND exist_in_UTokyo='No' AND (book_title NOT LIKE 'Not_found_in%' OR book_title NOT LIKE 'INVALID_ISBN')"\
+q -d, -O -H "SELECT $base_columns, exist_in_Sophia, exist_in_UTokyo \
+FROM ./bookmeter_wish_books.csv \
+WHERE exist_in_Sophia='No' AND exist_in_UTokyo='No' AND $filter_book_title_error"\
 > not_in_Sophia_and_UTokyo.csv
