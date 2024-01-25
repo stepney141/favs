dad2d25a0048b653e914fbe80ce76536ddbc19bb
stepney141 <stepney141andi.c.i@gmail.com>
2024-01-25 22:52:47 +0900

    fix: [bookmeter_wish] CSVカラムを利用しやすいよう修正

--- bookmeter_wish/is_in_my_lib.sh
+++ bookmeter_wish/is_in_my_lib.sh
@@ -27,19 +27,19 @@ WHERE exist_in_UTokyo='Yes' AND $filter_book_title_error"\
 > in_UTokyo.csv
 
 # 上智にあって東大にない
-q -d, -O -H "SELECT $base_columns \
+q -d, -O -H "SELECT $base_columns, central_opac_link \
 $source_from \
 WHERE exist_in_Sophia='Yes' AND exist_in_UTokyo='No' AND $filter_book_title_error"\
 > in_Sophia_but_not_in_UTokyo.csv
 
 # 東大にあって上智にない
-q -d, -O -H "SELECT $base_columns \
+q -d, -O -H "SELECT $base_columns, central_opac_link \
 $source_from \
 WHERE exist_in_Sophia='No' AND exist_in_UTokyo='Yes' AND $filter_book_title_error"\
 > in_UTokyo_but_not_in_Sophia.csv
 
 # 上智にも東大にもない
-q -d, -O -H "SELECT $base_columns, exist_in_Sophia, exist_in_UTokyo \
+q -d, -O -H "SELECT $base_columns \
 $source_from \
 WHERE exist_in_Sophia='No' AND exist_in_UTokyo='No' AND $filter_book_title_error"\
 > not_in_Sophia_and_UTokyo.csv
