c9597e3191df8d88fad5822f5babd1a162a78609
stepney141 <stepney141@users.noreply.github.com>
2025-06-17 10:49:35 +0000

    auto-updated: 2025-06-17T19:49+09:00

---
+++ constants.ts
@@ -0,0 +1,10 @@
+export const ACCOUNTS = {
+  jawiki: "ja.wikipedia.org",
+  enwiki: "en.wikipedia.org",
+  wikicommons: "commons.wikimedia.org",
+  jawikibooks: "ja.wikibooks.org",
+  jawikisource: "ja.wikisource.org",
+  enwikisource: "en.wikisource.org"
+} as const;
+
+export const JOB_NAME = "Wikipedia Watchlists";
