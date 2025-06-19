e29e66e587ea9759c8b24abd72c0bdbfb587de20
stepney141 <stepney141andi.c.i@gmail.com>
2025-06-19 16:49:44 +0900

    fix: update chrome args

--- .libs/constants.ts
+++ .libs/constants.ts
@@ -1,4 +1,12 @@
 export const USER_AGENT =
   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36";
 
-export const CHROME_ARGS = ["--no-sandbox", "--disable-setuid-sandbox"];
+export const CHROME_ARGS = [
+  "--disable-gpu",
+  "--disable-dev-shm-usage",
+  "--disable-setuid-sandbox",
+  "--no-first-run",
+  "--no-zygote",
+  "--single-process",
+  "--no-sandbox"
+];
